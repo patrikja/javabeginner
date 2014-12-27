d97b2f271fec8e26d32948a52f2924b08644d69d
Julia Jansson <julia.jansson@gmail.com>
2014-12-27 18:21:02 +0100

    This is a file showing my (fake) number :)

---
+++ javamapp/Callme.java
@@ -0,0 +1,9 @@
+import javax.swing. *;
+
+public class Callme {
+       public static void main (String[] arg) {
+       	      JOptionPane.showMessageDialog(null, "Hej, jag heter Julia Jansson ;)");
+       	      JOptionPane.showMessageDialog(null, "0123456789 ;)");
+       	      JOptionPane.showMessageDialog(null, "Call me ;)");
+}}
+      
